0ac02d1c57cc67e944fce653a71c513c1f503da1
20fer20 <72565453+20fer20@users.noreply.github.com>
2021-03-04 18:17:55 -0300

    Ejericicio Resuelto! FJ

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,1 +1,15 @@
 // @TODO Completar...
+const contenedor = document.getElementById('contenedor');
+
+var lista = new DocumentFragment()
+
+lista.forEach(function () {
+    for ( i=0, i == 50, i++){
+  var li = document.createElement('li')
+  li.innerHTML = lista
+  lista.appendChild(li)
+    }
+})
+
+list.appendChild(lista)
+
